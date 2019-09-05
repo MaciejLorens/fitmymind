89ff65ec7ee0d1d3c3ce32f4b10f3a50b2f57e12
Maciej Lorens <maciej.lorens@gmail.com>
2019-09-05 17:41:15 +0200

    fix fonts #2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( template/custom.js .svg .eot .woff .ttf fonts )
+Rails.application.config.assets.precompile += %w( template/custom.js )
