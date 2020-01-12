c632667b188a95196cb742c5970eebf730dd2c78
Maciej Lorens <maciej.lorens@gmail.com>
2020-01-12 14:37:46 +0100

    fix size of the photos on mobile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_attached_file :photo,
-                    styles: { big: '750x500', medium: '360x230', thumb: '80x53' },
+                    styles: { big: '945x450', medium: '750x357', thumb: '80x43' },
                     default_url: '/assets/missing/:style/missing.jpg'
 
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
